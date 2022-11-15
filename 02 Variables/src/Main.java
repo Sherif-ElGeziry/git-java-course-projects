624b019f245d350784495cdf37ba47352a9ce196
scs <scs@DESKTOP-Q3V98AT.home>
2022-11-15 21:59:24 +0200

    Variables difference

---
+++ 02 Variables/src/Main.java
@@ -0,0 +1,26 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		/*int x; //declaration
+		 * x = 123' // assignment
+		 * 
+		*long x = 1242454625636L;
+		*float y = 3.14f;
+		*/
+		
+		int x = 123; // initialization
+		double y = 3.14;
+		boolean z = true;
+		char symbol = '@';
+		String name = "Sherif";
+		
+		System.out.println("My number x is: " + x);
+		System.out.println("My number y is: " + y);
+		System.out.println("My boolean is: " + z);
+		System.out.println("My char is: " + symbol);
+		System.out.println("Hello " + name);
+	}
+
+}
