d6f2a3ad0a5471be68368f2d24c3092aa7ca181a
scs <scs@DESKTOP-Q3V98AT.home>
2022-11-15 19:39:28 +0200

    Print basics

---
+++ MyfirstProgram/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+
+	public static void main(String[] args) {
+		System.out.println("\\\t\"I love pizza\"");
+		System.out.println("It's really good!");
+		
+		//This is a coment
+		/*This
+		 * is
+		 * a
+		 * comment*/
+		System.out.println();//created by sysout + ctrl + space	
+	}
+
+}
