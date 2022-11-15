8dd7b08508bf772ea13d36ff9ef44b5b33defffb
scs <scs@DESKTOP-Q3V98AT.home>
2022-11-15 21:43:59 +0200

    First commit

---
+++ 01 MyfirstProject/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("///tI love pizza");
+		System.out.println("It's really good");
+
+		//This is a comment
+		
+		/*This
+		 * is also
+		 * a comment
+		 */
+		
+		System.out.println(); //Created with shortcut sysout + ctrl + spacebar
+	}
+
+}
