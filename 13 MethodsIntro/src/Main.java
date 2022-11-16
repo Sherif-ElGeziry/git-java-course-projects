db9b9a75eb40247c2208cd8539680f7ba18c081c
scs <scs@DESKTOP-Q3V98AT.home>
2022-11-16 06:03:30 +0200

    Math methods

--- 13 MethodsIntro/src/Main.java
+++ 13 MethodsIntro/src/Main.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println(Math.sqrt(25));
+
+		double i = 32;
+		System.out.println(Math.sqrt(i));
 
 	}
 
