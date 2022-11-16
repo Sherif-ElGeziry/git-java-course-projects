755ea9db68417c35b71ef7753a61b05a38aa183f
scs <scs@DESKTOP-Q3V98AT.home>
2022-11-16 15:56:06 +0200

    Overloading functions

--- 14 FunctionsProjects/src/Main.java
+++ 14 FunctionsProjects/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		int x, y, z, total;
+		/*int x, y, z, total;
 		double average;
 		
 		System.out.println("Enter first number: ");
@@ -22,9 +22,16 @@ public class Main {
 
 		System.out.println(globalVar);
 		int globalVar = 500;
-		System.out.println(globalVar);
+		System.out.println(globalVar);*/
 		//System.out.println(DisplayOutput. globalVar);
 		
+		//Overloading example
+		int n1 = 20, n2 = 12, n5 = 30;
+		char n3 = 'A', n4 = 'a';
+		
+		System.out.println(larger(n1, n2));
+		System.out.println(larger(n3, n4));
+		System.out.println(larger(n1, n2, n5));
 	}
 	
 	public static int sum(int n1, int n2, int n3) {
@@ -41,5 +48,28 @@ public class Main {
 	public static void display(int sum, double avr) {
 		System.out.println("The sum of the numbers is: " + sum);
 		System.out.println("The average of the numbers is: " + avr);
+	}
+	
+	public static int larger(int x, int y) {
+		if(x > y)
+			return x;
+		else
+			return y;
+	}
+	
+	public static int larger(int x, int y, int z) {
+		if(x > y && x > z)
+			return x;
+		else if (y > x && y > z)
+			return y;
+		else
+			return z;
+	}
+	
+	public static char larger(char x, char y) {
+		if(x>y)
+			return x;
+		else
+			return y;
 	}
 }
