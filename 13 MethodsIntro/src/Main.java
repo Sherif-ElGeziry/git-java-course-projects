c2b498c8b5bb6eef8e6d2b53f8d720fbdd26df93
scs <scs@DESKTOP-Q3V98AT.home>
2022-11-16 06:09:20 +0200

    introducing parameters

--- 13 MethodsIntro/src/Main.java
+++ 13 MethodsIntro/src/Main.java
@@ -11,15 +11,15 @@ public class Main {
 		System.out.println(Math.sqrt(i));*/
 		
 		System.out.println("Start of the project.");
-		drawLine();
+		drawLine('*', 15);
 		System.out.println("Welcome to the first part in functions.");
-		drawLine();
+		drawLine('#', 30);
 
 	}
 	
-	public static void drawLine() {
-		for(int i = 0; i < 10; i++)
-			System.out.print("*");
+	public static void drawLine(char ch, int length) {
+		for(int i = 0; i < length; i++)
+			System.out.print(ch);
 		System.out.println();
 	}
 
