81240aeba029fd6e412db71297c8953179167de5
scs <scs@DESKTOP-Q3V98AT.home>
2022-11-16 06:07:01 +0200

    created first function

--- 13 MethodsIntro/src/Main.java
+++ 13 MethodsIntro/src/Main.java
@@ -5,11 +5,22 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
-		System.out.println(Math.sqrt(25));
+		/*System.out.println(Math.sqrt(25));
 
 		double i = 32;
-		System.out.println(Math.sqrt(i));
+		System.out.println(Math.sqrt(i));*/
+		
+		System.out.println("Start of the project.");
+		drawLine();
+		System.out.println("Welcome to the first part in functions.");
+		drawLine();
 
+	}
+	
+	public static void drawLine() {
+		for(int i = 0; i < 10; i++)
+			System.out.print("*");
+		System.out.println();
 	}
 
 }
