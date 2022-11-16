2f7c30bcd05d1043f29661cdc1f5c3dcccd67446
scs <scs@DESKTOP-Q3V98AT.home>
2022-11-16 04:53:31 +0200

    Program applied

--- 11 Break&Continue/src/Main.java
+++ 11 Break&Continue/src/Main.java
@@ -1,9 +1,25 @@
-
+import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner reader = new Scanner(System.in);
+		int sum = 0, x;
+		
+		for(int i = 1; i <= 7; i++) {//i++ here makes counter continue even if else condition applied
+			System.out.println("Enter number" + i);
+			x = reader.nextInt();
+			
+			if(x > 0)
+				sum += x;
+			else
+				//if I use break instead of continue I exit for loop entirely
+				continue;
+			
+			//i++;
+			//If i++ here instead and we go in else condition then counter doesn't increment this loop
+		}
+		
+		System.out.println("The sum is:" + sum);
 	}
 
 }
