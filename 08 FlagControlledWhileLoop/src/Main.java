67c5ae3260fda96c7f01010ecaa80480ae81e9e8
scs <scs@DESKTOP-Q3V98AT.home>
2022-11-16 03:21:28 +0200

    Flag applied

--- 08 FlagControlledWhileLoop/src/Main.java
+++ 08 FlagControlledWhileLoop/src/Main.java
@@ -1,9 +1,22 @@
-
+import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner reader = new Scanner(System.in);
+		double x, sum = 0;
+		boolean nonNegative = true;
+		System.out.println("Enter only positive number to sum");
+		
+		while(nonNegative) {
+			System.out.println("Enter Number: ");
+			x = reader.nextInt();
+			if(x > 0)
+				sum += x;
+			else
+				nonNegative = false;
+		}
+		
+		System.out.println("The sum is: " + sum);
 	}
 
 }
