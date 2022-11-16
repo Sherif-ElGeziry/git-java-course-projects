65c5c122c2502ce4fe1a670e35f2fa90c6a34839
scs <scs@DESKTOP-Q3V98AT.home>
2022-11-16 23:34:52 +0200

    sum and average

--- 15 ArraysIntro/src/Main.java
+++ 15 ArraysIntro/src/Main.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter size of array: ");
+		int size = input.nextInt();
+		double[] salaries = new double[size];
+		//double[] sales = {12.34, 24.213, 12.43, 8.432};
+		
+		for(int i = 0; i < salaries.length; i++) {
+			System.out.println("Enter the salary of employee number " + (i + 1));
+			salaries[i] = input.nextDouble();
+		}
+		
+		double sum = 0;
+		for(int i = 0; i < salaries.length; i++) {
+			sum += salaries[i];
+		}
+		
+		System.out.println("The sum of the array is: " + sum);
+		System.out.println("The averge of the array is: " + (sum / size));
 	}
 
 }
