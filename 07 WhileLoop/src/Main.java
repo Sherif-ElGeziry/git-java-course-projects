c2f87a522568ed7aa6a3c7097be093929bf82bc6
scs <scs@DESKTOP-Q3V98AT.home>
2022-11-16 02:53:38 +0200

    Six students average

--- 07 WhileLoop/src/Main.java
+++ 07 WhileLoop/src/Main.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner grade = new Scanner(System.in);
+		int count = 1, sum = 0;
+		
+		while(count <= 6) {
+			System.out.println("Enter grade of student: " + count);
+			int num = grade.nextInt();
+			sum += num;
+			count++;
+		}
+		
+		System.out.println("The average grade is: " + sum / 6);
 	}
 
 }
