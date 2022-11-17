d58dbd116eb034e152d6d284c2ccb630388a83e9
scs <scs@DESKTOP-Q3V98AT.home>
2022-11-17 03:04:43 +0200

    Matrix project

--- 17 TwoDimensionalArrays/src/Main.java
+++ 17 TwoDimensionalArrays/src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
 
-	Scanner input = new Scanner(System.in);
+	static Scanner input = new Scanner(System.in);
 	
 	public static void main(String[] args) {
 		int [][] marks = new int[20][15];
@@ -21,6 +21,43 @@ public class Main {
 		for(int i = 0; i < marks2[2].length; i++) {
 			System.out.println(marks[2][i]);
 		}
+		
+		//New matrix project
+		int[][] matrix = new int[5][3];
+		fillArray(matrix);
+		printArray(matrix);
+		int total = sumArray(matrix);
+		System.out.println("Sum is: " + total);
 	}
 
+	public static void fillArray(int[][] list) {
+		System.out.println("Enter array data below");
+		for(int row = 0; row < list.length; row++){
+			for(int col = 0; col < list[0].length; col++) {
+				System.out.println("Enter data of row #" + (row + 1) + " and column #" + (col + 1));
+				list[row][col] = input.nextInt();
+			}
+		}
+	}
+	
+	public static void printArray(int[][] list) {
+		System.out.println("Array data will be printed below");
+		for(int row = 0; row < list.length; row++){
+			for(int col = 0; col < list[0].length; col++) {
+				System.out.print(list[row][col] + "\t");
+			}
+			System.out.println();
+		}
+	}
+	
+	public static int sumArray(int[][] list) {
+		System.out.println("Sum of all elements will be printed below");
+		int sum = 0;
+		for(int row = 0; row < list.length; row++){
+			for(int col = 0; col < list[0].length; col++) {
+				sum += list[row][col];
+			}
+		}
+		return sum;
+	}
 }
