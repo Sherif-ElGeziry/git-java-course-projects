0adf1b0cc5a76d604108edf0a80e1a46cbda1441
scs <scs@DESKTOP-Q3V98AT.home>
2022-11-19 17:20:31 +0200

    Exception handling

--- 22 EnumeratedTypes/src/Main.java
+++ 22 EnumeratedTypes/src/Main.java
@@ -1,7 +1,10 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
+		// Enum testing
 		Course c1 = Course.Maths;
 		Course c2 = Course.Maths;
 		Course c3 = Course.Database;
@@ -13,6 +16,29 @@ public class Main {
 		System.out.println(c3.compareTo(c2));
 		System.out.println(c1.equals(c2));
 		System.out.println(c1.equals(c3));
+		System.out.println();
+
+		// Exception handling
+		Scanner s = new Scanner(System.in);
+		try {
+			// int x = 10, y = 0;
+			System.out.println("Enter x");
+			int x = s.nextInt();
+
+			System.out.println("Enter y");
+			int y = s.nextInt();
+
+			System.out.println(x / y);
+		} catch (ArithmeticException e) {
+			System.out.println("Error, Division by zero is not allowed");
+		} catch (InputMismatchException e) {
+			System.out.println("Error, Enter int numbers only");
+		} catch (Exception e) {
+			// TODO: handle any unexpected exceptions
+			System.out.println(e.getMessage());
+		}
+
+		System.out.println("The program continues");
 	}
 
 }
